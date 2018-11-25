49e60a24a92442d702f5d69932ab56378b1dd456
Huynh Duc Khoan <admin@mrsoi.com>
2018-11-25 14:35:35 +0700

    Update version 0.0.1 : Fix bug error ajax

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,15 +2,23 @@ $(document).ready(function(){
     $("#btn-shorter").click(function(){
         var $this = $(this);
         $this.button('loading');
-        $.post("do",
-        {
-            url: $('#long-url').val(),
-        },
-        function(data,status){
-            console.log(data);
-            $('#short-url').html(data.short_url);
-            $('#short-url').removeClass("hidden");
-            $this.button('reset');
-        });
+        $.ajax({
+            type: "POST",
+            url: "do",
+            data: {
+                url: $('#long-url').val(),
+            },
+            success: function(data){
+                console.log(data);
+                $('#short-url').html(data.short_url);
+                $('#short-url').removeClass("hidden");
+                $this.button('reset');
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                $this.button('reset');
+                alert(XMLHttpRequest.responseJSON.message);
+                // alert("Wow... " + $('#long-url').val() + " is not a valid url!!!");
+            }
+          });
     });
 });
